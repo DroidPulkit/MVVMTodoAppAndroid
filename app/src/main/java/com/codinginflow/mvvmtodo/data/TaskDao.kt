cd11877906cc0254cf2ebbf4ab710d0786cb5bc3
droidpulkit <pulkitkumar190@gmail.com>
2021-04-09 00:58:43 -0400

    Room + HILT DI

---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDao.kt
@@ -0,0 +1,21 @@
+package com.codinginflow.mvvmtodo.data
+
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TaskDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+
+    @Delete
+    suspend fun delete(task: Task)
+
+    @Query("Select * from  task_table")
+    fun getAllTasks() : Flow<List<Task>>
+
+}
---
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/TaskDatabase.kt
@@ -0,0 +1,40 @@
+package com.codinginflow.mvvmtodo.data
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.codinginflow.mvvmtodo.di.ApplicationScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+import javax.inject.Provider
+
+@Database(entities = [Task::class], version = 1)
+abstract class TaskDatabase : RoomDatabase() {
+
+    abstract fun taskDao() : TaskDao
+
+    class Callback @Inject constructor(
+        private val database: Provider<TaskDatabase>,
+        @ApplicationScope private val applicationScope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+
+            //db operations
+            val dao = database.get().taskDao()
+
+            applicationScope.launch {
+                dao.insert(Task("Test 1", isImportant = true))
+                dao.insert(Task("Test 2", isCompleted = true))
+                dao.insert(Task("Test 3"))
+                dao.insert(Task("Test 4"))
+                dao.insert(Task("Test 5"))
+                dao.insert(Task("Test 6", isCompleted = true, isImportant = true))
+            }
+
+        }
+    }
+}
